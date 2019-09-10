49a394932fda29a55c50923b77832afa255efa52
Lialah Barrer <lrbarrer@gmail.com>
2019-09-10 19:45:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,9 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
+def speak_to_grandma(phrase)
+  phrase = #{phrase} 
   if  "I love you grandma"
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif "I LOVE YOU GRANDMA"
-    puts "I LOVE YOU TOO PUMPKIN"
+  elsif "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
 end
