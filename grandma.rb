f3bda32aac68bc7655c7afc785875392c8f6cb33
Lialah Barrer <lrbarrer@gmail.com>
2019-09-10 19:43:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,4 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma
+  if  "I love you grandma"
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif "I LOVE YOU GRANDMA"
+    puts "I LOVE YOU TOO PUMPKIN"
+    
 end
